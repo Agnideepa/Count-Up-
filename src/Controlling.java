9d259757450c6b3e6d4f44533d06d8364ba68a5f
Agnideepa <45922615+Agnideepa@users.noreply.github.com>
2018-12-17 17:48:41 +0000

    Update Controlling.java

--- src/Controlling.java
+++ src/Controlling.java
@@ -1,4 +1,5 @@
-/**This is essentially an interface to represent a controller where the View makes use of a SelectionPanel, AnswerPanel
+/**
+ * This is essentially an interface to represent a controller where the View makes use of a SelectionPanel, AnswerPanel
  * and a ChoicePanel. There has to be a start method and the basic idea must remain the same : 2 choices selected, an
  * answer selected and an operation selected. It is up to the implementing class to decide what to do once any of these
  * events has occurred.
